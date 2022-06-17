7e8113fbd18b0f05bffd983be24fcd5d063c2a55
Lukas <lukj@itu.dk>
2022-06-17 10:34:09 +0200

    Project running with bs and fa

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Modal } from 'react-bootstrap';
+import FontAwesome from 'react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Modal show={true}>
+        <FontAwesomeIcon icon={faCoffee}></FontAwesomeIcon>
+      </Modal>
     </div>
   );
 }
